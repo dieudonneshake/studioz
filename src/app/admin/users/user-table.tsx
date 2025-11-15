d96eb1f8f3538bf6f7c46c3b9a458615c43a8444
Dieudonné UWIMANA <dieudoneuwimana@gmail.com>
2025-11-15 19:34:49 +0000

    The manage users page is loading..  And never finish

--- src/app/admin/users/user-table.tsx
+++ src/app/admin/users/user-table.tsx
@@ -1,4 +1,6 @@
 
+"use client";
+
 import { Badge } from "@/components/ui/badge";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { type User } from "@/lib/types";
