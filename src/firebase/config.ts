cf600747c51a87ea4fb1568a1aa0f1897b95aac3
Dieudonné UWIMANA <dieudoneuwimana@gmail.com>
2025-11-26 15:20:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "impactflow-ewggi",
+  "appId": "1:258986924111:web:599139d13ecfe564dfb23a",
+  "apiKey": "AIzaSyB3QJEyF-sahZ4eN5DFlfndNnmwvSs3Kyo",
+  "authDomain": "impactflow-ewggi.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "258986924111"
+};
